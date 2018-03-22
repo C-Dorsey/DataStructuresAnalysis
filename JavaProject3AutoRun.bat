00368e568b2eb965f44cc4aa86dc3fe4feeb4ca0
Cohl Dorsey <37632573+C-Dorsey@users.noreply.github.com>
2018-03-22 01:37:28 -0500

    Add files via upload

---
+++ JavaProject3AutoRun.bat
@@ -0,0 +1,73 @@
+@echo off
+GOTO A
+:A
+echo -----------------------------
+echo 1st
+echo -----------------------------
+echo -------------- 1st ------------- >>Outputs3.txt
+java NumberCruncher >> Outputs3.txt
+GOTO B
+:B
+echo -----------------------------
+echo 2nd
+echo -----------------------------
+echo -------------- 2nd ------------- >>Outputs3.txt
+java NumberCruncher >> Outputs3.txt
+GOTO C
+:C
+echo -----------------------------
+echo 3rd
+echo -----------------------------
+echo -------------- 3rd ------------- >>Outputs3.txt
+java NumberCruncher >> Outputs3.txt
+GOTO D
+:D
+echo -----------------------------
+echo 4th
+echo -----------------------------
+echo -------------- 4th ------------- >>Outputs3.txt
+java NumberCruncher >> Outputs3.txt
+GOTO E
+:E
+echo -----------------------------
+echo 5th
+echo -----------------------------
+echo -------------- 5th ------------- >>Outputs3.txt
+java NumberCruncher >> Outputs3.txt
+GOTO F
+:F
+echo -----------------------------
+echo 6th
+echo -----------------------------
+echo -------------- 6th ------------- >>Outputs3.txt
+java NumberCruncher >> Outputs3.txt
+GOTO G
+:G
+echo -----------------------------
+echo 7th
+echo -----------------------------
+echo -------------- 7th ------------- >>Outputs3.txt
+java NumberCruncher >> Outputs3.txt
+GOTO H
+:H
+echo -----------------------------
+echo 8th
+echo -----------------------------
+echo -------------- 8th ------------- >>Outputs3.txt
+java NumberCruncher >> Outputs3.txt
+GOTO I
+:I
+echo -----------------------------
+echo 9th
+echo -----------------------------
+echo -------------- 9th ------------- >>Outputs3.txt
+java NumberCruncher >> Outputs3.txt
+GOTO J
+:J
+echo -----------------------------
+echo 10th
+echo -----------------------------
+echo -------------- 10th ------------- >>Outputs3.txt
+java NumberCruncher >> Outputs3.txt
+
+EXIT
